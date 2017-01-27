befd38f860611f68045a68e6974d94f93570b9c3
Benjamin Tamási <h@lfto.me>
2017-01-27 15:07:03 +0100

    Ant stuff

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import json
 import sys
 
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
+from libAnt.drivers.pcap import PcapDriver
+from libAnt.node import Node
+from libAnt.profiles.factory import Factory as AntFactory
 from twisted.internet import reactor
 from twisted.python import log
 
@@ -9,6 +12,12 @@ from twisted.python import log
 class MyServerProtocol(WebSocketServerProtocol):
     def __init__(self):
         super().__init__()
+        self.antFactory = AntFactory(self.onAntMessage)
+        #self.antFactory.enableFilter()
+        #self.antFactory.addToFilter(11111)
+        self.node = Node(PcapDriver("dummy.cap"), 'PcapNode')
+        self.node.enableRxScanMode()
+        self.node.start(self.antFactory.parseMessage, self.onAntErrorMessage)
 
     def sendJsonMessage(self, msg):
         payload = json.dumps(msg, ensure_ascii=False).encode('utf8')
@@ -32,6 +41,23 @@ class MyServerProtocol(WebSocketServerProtocol):
             except Exception as e:
                 print(e)
 
+    def onMeshMessage(self, payload):
+        try:
+            obj = json.loads(payload.decode('utf8'))
+            for key, value in obj.items():
+                try:
+                    getattr(self, 'onMeshCommand' + key)(value)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
+
+    def onAntMessage(self, payload):
+        pass
+
+    def onAntErrorMessage(self, error):
+        pass
+
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
 
@@ -39,6 +65,15 @@ class MyServerProtocol(WebSocketServerProtocol):
         pass
 
     def onCommandStopRace(self, data=None):
+        pass
+
+    def onMeshCommandStartRace(self, data=None):
+        pass
+
+    def onMeshCommandStopRace(self, data=None):
+        pass
+
+    def onMeshCommandUpdate(self, data=None):
         pass
 
 log.startLogging(sys.stdout)
