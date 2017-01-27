39170697d16a10b68a24c45e21a196eca5046e7e
Benjamin Tamási <h@lfto.me>
2017-01-27 15:27:24 +0100

    Add mesh

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 import json
+import socket
+import time
+import zmq
 import sys
+from threading import Lock, Thread
 
+import zmq
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
 from libAnt.drivers.pcap import PcapDriver
 from libAnt.node import Node
@@ -9,9 +14,32 @@ from twisted.internet import reactor
 from twisted.python import log
 
 
+class meshLoop(Thread):
+    def __init__(self, socket, callback):
+        super().__init__()
+        self.sock = socket
+        self.callback = callback
+
+    def run(self):
+        while True:
+            msg, addrinfo = self.sock.recvfrom(5000)  # pick a suitable size :S
+            try:
+                self.callback(msg)
+            except Exception as e:
+                print(e)
+
 class MyServerProtocol(WebSocketServerProtocol):
+
     def __init__(self):
         super().__init__()
+        self.lock = Lock()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.sock.bind(('', 9999))
+        self.mesh = meshLoop(self.sock, self.onMeshMessage)
+        self.mesh.start()
+        self.poller = zmq.Poller()
+        self.poller.register(self.sock, zmq.POLLIN)
         self.antFactory = AntFactory(self.onAntMessage)
         #self.antFactory.enableFilter()
         #self.antFactory.addToFilter(11111)
@@ -22,6 +50,10 @@ class MyServerProtocol(WebSocketServerProtocol):
     def sendJsonMessage(self, msg):
         payload = json.dumps(msg, ensure_ascii=False).encode('utf8')
         self.sendMessage(payload)
+
+    def sendJsonMeshMessage(self, msg):
+        payload = json.dumps(msg, ensure_ascii=False).encode('utf8')
+        self.sock.sendto(payload, 0, ("255.255.255.255", 9999))
 
     def onConnect(self, request):
         print("Client connecting: {0}".format(request.peer))
